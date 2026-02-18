c6ba94406ffd2a6b836c2a976e8601c5a7acc07c
dipsana24 <dipsanaadhk2403@gmail.com>
2026-02-18 10:55:01 +0545

    Fix: correct base path and script src for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/reactt_attendance/",
   plugins: [react()],
-  base: '/reactt_attendance/',   // MUST match your repo name
-})
+});
