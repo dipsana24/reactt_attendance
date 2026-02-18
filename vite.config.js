25cf8b406305c179699471542bb519656786adc4
dipsana24 <dipsanaadhk2403@gmail.com>
2026-02-18 11:26:09 +0545

    Fix: correct base path typo in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/reactt_attendance/",
   plugins: [react()],
+  base: "/reactt_attendance/", // EXACT repo name
 });
