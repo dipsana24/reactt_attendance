7d42cd82b2a6353a7239b3b877ca296dc7c5ab34
dipsana24 <dipsanaadhk2403@gmail.com>
2026-02-26 09:23:02 +0545

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/reactt_attendance/" : "/",
-  // EXACT repo name
+  base: "/attendance-app/", // 👈 IMPORTANT: match your repo name exactly
 });
